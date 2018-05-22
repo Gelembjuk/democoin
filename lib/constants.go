77cf8850b31d6b7515c629cf3be31cc229484d17
Roman Gelembjuk <roman@gelembjuk.com>
2018-05-22 06:55:37 +0300

    increase version

--- lib/constants.go
+++ lib/constants.go
@@ -1,7 +1,7 @@
 package lib
 
 const ApplicationTitle = "DemoCoin"
-const ApplicationVersion = "0.1 beta"
+const ApplicationVersion = "0.2 beta"
 
 const Version = byte(0x00)
 const AddressChecksumLen = 4
